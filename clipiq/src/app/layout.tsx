23c5cd23ef3cd2f85479b2ec2b3c942815edf124
utkarsh-2033 <utkarshg@2033>
2025-06-03 01:56:18 +0530

    add Prisma dependency and integrate Clerk and Theme providers; remove old page component

--- clipiq/src/app/layout.tsx
+++ clipiq/src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme";
+
 import "./globals.css";
 
-const manrope = Manrope({subsets: ["latin"]});
+const manrope = Manrope({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ClipIQ",
@@ -15,12 +18,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${manrope.className} bg-[#171717]`}
-      >
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`${manrope.className} bg-[#171717]`}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
