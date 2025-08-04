80f88009ca851b1f7b735d62cf09cd6b55496d90
utkarsh-2033 <utkarshg@2033>
2025-08-05 02:43:50 +0530

    clip iq desktop app download link added and auth layout updated

--- clipiq/src/app/auth/layout.tsx
+++ clipiq/src/app/auth/layout.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const Layout = ({ children }:Props) => {
-  return <div className='flex items-center justify-center min-h-screen container'>
+  return <div className='flex items-center justify-center min-h-screen bg-gray-900'>
     {children}</div>;
 };
 
--- clipiq/src/components/global/topbar/record.tsx
+++ clipiq/src/components/global/topbar/record.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Modal from "@/components/global/modal";
 import { Button } from "@/components/ui/button";
-import { Video } from "lucide-react";
+import { Download, Video } from "lucide-react";
+import Link from "next/link";
+import { toast } from "sonner";
 
 const Record = () => {
   return (
@@ -12,20 +14,20 @@ const Record = () => {
           <span className="flex items-center gap-2 font-bold">Record</span>
         </Button>
       }
-      title="Clip IQ Desktop App - Record"
-      description="Record your video using the Clip IQ desktop app"
+      title="Clip IQ Desktop App - Screen Recorder"
+      description="Download and record your video using the Clip IQ desktop app. The video gets uplaoded in real time automatically to your workspace."
     >
-      <p>
-        Due to auth platform (Clerk/OAuth) security restrictions, true OAuth
-        login cannot work in a packaged Electron build without a custom domain
-        and production credentials which required custom domain and does not
-        accept vercel domains.
-      </p>
-      <p className="mt-4 text-blue-300 hover:underline">
-        <a href="https://github.com/utkarsh-2033/ClipIQ-desktop-app">
-          GitHub Link
-        </a>
-      </p>
+      <div className="flex flex-col items-center justify-center mt-4 gap-4">
+        <Link
+        onClick={() => { toast.success("Download Started...") }}
+          target="_blank" 
+          className="flex items-center justify-center gap-2 bg-purple-600 text-white p-2 rounded-md"
+          href="https://github.com/utkarsh-2033/ClipIQ-desktop-app/releases/download/v0.0.1/ClipIQ.Recorder.Setup.0.0.0.exe"
+        >
+          {" "}
+          <Download /> Download App
+        </Link>
+      </div>
     </Modal>
   );
 };
